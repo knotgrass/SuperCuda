6f408fa2509a55e39c4380f5db8e7ecdf59e7d3d
agent12 <imnova1212@gmail.com>
2023-07-07 17:32:06 +0700

    cudnn-linux-x86_64-8.9.2.26_cuda11

---
+++ env/cudnn.sh
@@ -0,0 +1,9 @@
+
+# install-guide
+# https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html#installlinux-tar
+# https://developer.nvidia.com/rdp/cudnn-download
+
+
+url="https://developer.nvidia.com/downloads/compute/cudnn/secure/8.9.2/local_installers/11.x/cudnn-linux-x86_64-8.9.2.26_cuda11-archive.tar.xz/"
+# pip install nvidia-cudnn-cu11==8.6.0.163 # tensorflow 2.12
+pip install nvidia-cudnn-cu11==8.9.2.26
