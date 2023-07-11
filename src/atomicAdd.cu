330f28e1f422c21fbd67bfc30176d87454b62476
agent12 <imnova1212@gmail.com>
2023-07-11 23:18:07 +0700

    cudaMemcpyFromSymbol

---
+++ src/atomicAdd.cu
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <assert.h>
+#include <cuda_runtime.h>
+
+
+__device__ float devResult = 0.0f;
+
+__global__ void vectorSum(const float* input, int size){
+    const int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid < size)
+        atomicAdd(&devResult, input[tid]);
+}
+
+int main(int argc, char** argv) {
+    const int size = 10;
+    const int threadsPerBlock = 256;
+    const int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+
+    float* input = new float[size];
+
+    // Khởi tạo vector đầu vào
+    for (int i = 0; i < size; ++i) {
+        input[i] = 1.0f;
+    }
+
+    // Cấp phát bộ nhớ trên thiết bị
+    float* devInput;
+    cudaMalloc((void**)&devInput, sizeof(float) * size);
+
+    // Sao chép dữ liệu từ host sang device
+    cudaMemcpy(devInput, input, sizeof(float) * size, cudaMemcpyHostToDevice);
+    cudaMemcpyToSymbol(devResult, &devResult, sizeof(float));
+
+    // Gọi kernel
+    vectorSum<<<blocksPerGrid, threadsPerBlock>>>(devInput, size);
+
+    // Sao chép kết quả từ device sang host
+    cudaMemcpyFromSymbol(&devResult, devResult, sizeof(float));
+
+    // In kết quả
+    std::cout << "Tổng của vector: " << devResult << std::endl;
+    assert(fabs(float(size) - devResult) <= 1e-6);
+
+    // Release memory
+    cudaFree(devInput);
+    delete[] input;
+
+    return 0;
+}
