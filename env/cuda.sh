68ce32b24f5aac0cd25fba7a80beb404ef05b57d
knotgrass <40136779+knotgrass@users.noreply.github.com>
2024-07-04 23:12:38 +0700

    add nvcc4jupyter

--- env/cuda.sh
+++ env/cuda.sh
@@ -5,3 +5,5 @@ CONDAENV="cxx"    # edit conda env name
 
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#installing-previous-cuda-releases
 conda install cuda -c nvidia/label/cuda-11.8.0 --name "$CONDAENV" -y
+
+pip install nvcc4jupyter
