22feca84162abb89cd8aef32aec916b328d0b904
knotgrass <40136779+knotgrass@users.noreply.github.com>
2024-12-24 23:44:02 +0700

    update cuda=12.4.1 cudnn=9.6.0.74 py3.12.8

--- env/cuda.sh
+++ env/cuda.sh
@@ -4,6 +4,7 @@ CONDAENV="cxx"    # edit conda env name
 # conda install -c conda-forge cudatoolkit-dev --force-reinstall --name "$CONDAENV" -y
 
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#installing-previous-cuda-releases
-conda install cuda -c nvidia/label/cuda-11.8.0 --name "$CONDAENV" -y
-
-pip install nvcc4jupyter
+conda install cuda -c nvidia/label/cuda-12.4.1 --name "$CONDAENV" -y
+# pytorch 2.5.1
+conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia
+pip install nvcc4jupyter -U --no-cache-dir --force-reinstall
--- env/cudnn.sh
+++ env/cudnn.sh
@@ -6,4 +6,6 @@
 
 url="https://developer.nvidia.com/downloads/compute/cudnn/secure/8.9.2/local_installers/11.x/cudnn-linux-x86_64-8.9.2.26_cuda11-archive.tar.xz/"
 # pip install nvidia-cudnn-cu11==8.6.0.163 # tensorflow 2.12
-pip install nvidia-cudnn-cu11==8.9.2.26
+# pip install nvidia-cudnn-cu11==8.9.2.26
+pip install nvidia-cudnn-cu12 -U #cuda 12.4.1 pytorch 2.5.1
+
